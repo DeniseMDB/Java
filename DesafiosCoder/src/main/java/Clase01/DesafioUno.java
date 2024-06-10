8183068bc7174e21221d537531ad600dcaee6946
unknown <denisedubois93@gmail.com>
2024-06-10 11:47:32 -0300

    Agregados desafios 1 y 2

---
+++ DesafiosCoder/src/main/java/Clase01/DesafioUno.java
@@ -0,0 +1,24 @@
+package Clase01;
+
+import java.util.Scanner;
+
+public class DesafioUno {
+    public static void main(String[] args) {
+        /*
+        Divide un numero float a su parte entera
+        y su parte decimal
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero decimal: ");
+        float numero = scanner.nextFloat();
+
+        int porcionEntera = (int)numero;
+        float porcionDecimal = numero - porcionEntera;
+
+        System.out.println("La porcion entera es: " + porcionEntera);
+        System.out.println("La porcion decimal es: " + porcionDecimal);
+    }
+
+
+}
---
+++ DesafiosCoder/src/main/java/Clase02/Main.java
@@ -0,0 +1,39 @@
+package Clase02;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Persona> personas = new ArrayList<>();
+
+        personas.add(new Persona("Pepito", "Suarez"));
+        personas.add(new Persona("Maria","Delbarrio"));
+        personas.add(new Persona("Federico", "Martinez"));
+        personas.add(new Persona("Florencia", "Rodriguez"));
+        personas.add(new Persona("Juanito", "Perez"));
+
+        // Ordenar y mostrar la lista por nombre
+        personas.sort(Comparator.comparing(Persona::getNombre));
+        System.out.println("Personas de la lista ordenada por nombre:");
+        for (Persona persona : personas) {
+            System.out.println(persona.getNombre());
+        }
+
+        // Ordenar y mostrar la lista por apellido
+        personas.sort(Comparator.comparing(Persona::getApellido));
+        System.out.println("\nPersonas de la lista ordenada por apellido:");
+        for (Persona persona : personas) {
+            System.out.println(persona.getApellido());
+        }
+
+        // Ordenar y mostrar la lista por apellido de forma inversa
+        personas.sort(Comparator.comparing(Persona::getApellido).reversed());
+        System.out.println("\nPersonas de la lista ordenada inversamente por apellido:");
+        for (Persona persona : personas) {
+            System.out.println(persona.getApellido());
+        }
+    }
+}
